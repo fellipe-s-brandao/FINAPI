7b8937c15a12b4f9767a3a6a7e86350dfc0f714d
Fellipe Brandao <fellipesilverio31@hotmail.com>
2022-01-17 21:53:35 -0300

    inicio da construção - cadastro de cliente e verificação se ja existe cpf cadastrado

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ---
 
 ### Requisitos 
-- [] Deve ser possível criar conta
+- [x] Deve ser possível criar conta
 - [] Deve ser possível buscar extrato bancário
 - [] Deve ser possível realizar depósito
 - [] Deve ser possível realizar saque
@@ -13,8 +13,8 @@
 - [] Deve ser possível atualizar dados da conta do cliente
 - [] Deve ser possível obter dados da conta do cliente 
 
-## Regras de negócio 
-- [] Não deve ser possível cadastrar uma conta com o CPF existente 
+### Regras de negócio 
+- [x] Não deve ser possível cadastrar uma conta com o CPF existente 
 - [] Não deve ser possível fazer depósito em uma conta não existente
 - [] Não deve ser possível fazer saque em uma conta não existente 
 - [] Não deve ser possível excluir uma conta não existente 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+    
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlreadyExists) {
+        return response.status(400).json({ erro: 'Customer already exists!'})
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    //201 - criado
+    return response.status(201).send("Conta criada com suceso")
+})
 
 app.listen(3333);
