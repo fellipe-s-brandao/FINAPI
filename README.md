4fe9203d3a685f1f41eb7876f574d6dbcf5efeb4
Fellipe Brandao <fellipesilverio31@hotmail.com>
2022-01-18 21:53:11 -0300

    rotas de saque e deposito criado

--- README.md
+++ README.md
@@ -7,15 +7,15 @@
 ### Requisitos 
 - [x] Deve ser possível criar conta
 - [x] Deve ser possível buscar extrato bancário
-- [] Deve ser possível realizar depósito
-- [] Deve ser possível realizar saque
+- [x] Deve ser possível realizar depósito
+- [x] Deve ser possível realizar saque
 - [] Deve ser possível buscar o extrato bancário do cliente por data
 - [] Deve ser possível atualizar dados da conta do cliente
 - [] Deve ser possível obter dados da conta do cliente 
 
 ### Regras de negócio 
 - [x] Não deve ser possível cadastrar uma conta com o CPF existente 
-- [] Não deve ser possível fazer depósito em uma conta não existente
-- [] Não deve ser possível fazer saque em uma conta não existente 
+- [x] Não deve ser possível fazer depósito em uma conta não existente
+- [x] Não deve ser possível fazer saque em uma conta não existente 
+- [x] Não deve ser possível fazer saque quando o salfo é insuficiente  
 - [] Não deve ser possível excluir uma conta não existente 
-- [] Não deve ser possível fazer saque quando o saldo for insuficiente 
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const customers = [];
 
 //middleware
 function verifyIfExistsAccountCPF(request, response, next) {
-    const { cpf } = request.params;
+    const { cpf } = request.headers;
 
     //retorna oq a gente precisa
     const customer = customers.find(costumer => costumer.cpf === cpf);
@@ -21,6 +21,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     request.customer = customer;
 
     return next();
+}
+
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit'){
+            return acc + operation.amount;
+        }else{
+            return acc - operation.amount
+        }
+    }, 0);
+
+    return balance;
 }
 
 /**
@@ -52,10 +64,49 @@ app.post('/account', (request, response) => {
     return response.status(201).send("Conta criada com suceso")
 });
 
-app.get('/statement/:cpf', verifyIfExistsAccountCPF, (request, response) => {
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
 
     return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount){
+        return response.status(400).json({ erro: 'Insufficient  founds!'});
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+
 });
 
 app.listen(3333);
